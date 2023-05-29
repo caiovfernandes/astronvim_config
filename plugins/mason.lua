21004509511d48d0ad828e627fbd372fa3b27537
Caio Fernandes <caiovf13@gmail.com>
2023-05-29 18:11:44 -0300

    📦 NEW: Add plugings configurations

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,24 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "quick_lint_js",
+        "jsonls",
+        "helm_ls",
+        "dockerls",
+        "docker_compose_language_service",
+        "jedi_language_server",
+        "powershell_es",
+        "bashls",
+        "azure_pipelines_ls",
+        "cssls",
+        "html",
+        "marksman",
+        "pylsp",
+        "sqlls",
+        "terraformls",
+        "yamlls",
+        "lemminx",
       })
     end,
   },
@@ -18,8 +35,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
       })
     end,
   },
@@ -29,7 +46,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
